XM5jDcsHTyGJtQqlCi/sbtc

commit 73a9edc53db858cd35c7159a4745bab06f9f874b

feat: add delay to processing of new Bitcoin blocks (#714)
* feat: add delay to processing of new blocks

* Add unit test

* Set delay from configuration

* Cleanup, no need to reformat

* Use `std::time::Duration`

* Add the delay _before_ DKG

* Cap max delay

* Make it less ugly

* Move validation to the right place

* Add TODO(715)

signer/src/config/mod.rs
@@ -11,6 +11,7 @@ use std::path::Path;
 use url::Url;
 
 use crate::config::error::SignerConfigError;
+use crate::config::serialization::duration_seconds_deserializer;
 use crate::config::serialization::p2p_multiaddr_deserializer_vec;
 use crate::config::serialization::parse_stacks_address;
 use crate::config::serialization::private_key_deserializer;
@@ -22,6 +23,8 @@ use crate::stacks::wallet::SignerWallet;
 
 mod error;
 mod serialization;
+
+pub const MAX_BITCOIN_PROCESSING_DELAY_S: u64 = 300;
 
 /// Trait for validating configuration values.
 trait Validatable {
@@ -234,6 +237,11 @@ pub struct SignerConfig {
     /// The number of signatures required for the signers' bootstrapped
     /// multi-sig wallet on Stacks.
     pub bootstrap_signatures_required: u16,
+    /// The number of seconds the coordinator will wait
+    /// before processing a new Bitcoin block
+    /// (allowing it to propagate to the others signers)
+    #[serde(deserialize_with = "duration_seconds_deserializer")]
+    pub bitcoin_processing_delay: std::time::Duration,
 }
 
 impl Validatable for SignerConfig {
@@ -258,6 +266,17 @@ impl Validatable for SignerConfig {
         // `SignerWallet::load_boostrap_wallet` function.
         if let Err(err) = SignerWallet::load_boostrap_wallet(self) {
             return Err(ConfigError::Message(err.to_string()));
+        }
+
+        let delay_secs = cfg.signer.bitcoin_processing_delay.as_secs();
+        if delay_secs > MAX_BITCOIN_PROCESSING_DELAY_S {
+            return Err(ConfigError::Message(
+                SignerConfigError::InvalidBitcoinProcessingDelay(
+                    MAX_BITCOIN_PROCESSING_DELAY_S,
+                    delay_secs,
+                )
+                .to_string(),
+            ));
         }
 
         // db_endpoint note: we don't validate the host because we will never
@@ -595,6 +614,15 @@ mod tests {
             Some(url::Host::Ipv6(ip))
         );
         assert_eq!(settings.stacks.endpoints[0].port(), Some(9101));
+
+        let delay = 42;
+        std::env::set_var("SIGNER_SIGNER__BITCOIN_PROCESSING_DELAY", delay.to_string());
+
+        let settings = Settings::new_from_default_config().unwrap();
+        assert_eq!(
+            settings.signer.bitcoin_processing_delay,
+            std::time::Duration::from_secs(delay),
+        );
     }
 
     #[test]
@@ -620,6 +648,21 @@ mod tests {
         assert!(matches!(
             settings.unwrap_err(),
             ConfigError::Message(msg) if msg == SignerConfigError::InvalidStacksPrivateKeyLength(4).to_string()
+        ));
+    }
+
+    #[test]
+    fn invalid_bitcoin_processing_delay_returns_correct_error() {
+        clear_env();
+
+        let delay = MAX_BITCOIN_PROCESSING_DELAY_S + 1;
+        std::env::set_var("SIGNER_SIGNER__BITCOIN_PROCESSING_DELAY", delay.to_string());
+
+        let settings = Settings::new_from_default_config();
+        assert!(settings.is_err());
+        assert!(matches!(
+            settings.unwrap_err(),
+            ConfigError::Message(msg) if msg == SignerConfigError::InvalidBitcoinProcessingDelay(MAX_BITCOIN_PROCESSING_DELAY_S, delay).to_string()
         ));
     }
 

signer/src/main.rs
@@ -369,14 +369,14 @@ async fn run_transaction_signer(ctx: impl Context) -> Result<(), Error> {
 
 /// Run the transaction coordinator event-loop.
 async fn run_transaction_coordinator(ctx: impl Context) -> Result<(), Error> {
-    let config = ctx.config().clone();
+    let private_key = ctx.config().signer.private_key.clone();
     let network = P2PNetwork::new(&ctx);
 
     let coord = transaction_coordinator::TxCoordinatorEventLoop {
         network,
         context: ctx,
         context_window: 10000,
-        private_key: config.signer.private_key,
+        private_key: private_key,
         signing_round_max_duration: Duration::from_secs(10),
         threshold: 2,
         dkg_max_duration: Duration::from_secs(10),

signer/src/config/default.toml
@@ -1,3 +1,4 @@
+# TODO(715): Provide sane/safe configuration defaults. Re-review all of them!
 # TODO(429): Add documentation for all configuration parameters.
 
 # !! ==============================================================================
@@ -91,6 +92,7 @@ endpoints = ["http://localhost:20443"]
 
 # This is the start height of the first EPOCH 3.0 block on the stacks
 # blockchain.
+# TODO(715): Update to 867867
 nakamoto_start_height = 31
 
 # !! ==============================================================================
@@ -108,6 +110,7 @@ nakamoto_start_height = 31
 # Format: "<hex-encoded-private-key>" (64 or 66 hex-characters)
 # Required: true
 # Environment: SIGNER_SIGNER__PRIVATE_KEY
+# TODO(715): Remove default value.
 private_key = "41634762d89dfa09133a4a8e9c1378d0161d29cd0a9433b51f1e3d32947a73dc"
 
 # Specifies which network to use when constructing and sending transactions
@@ -119,11 +122,13 @@ private_key = "41634762d89dfa09133a4a8e9c1378d0161d29cd0a9433b51f1e3d32947a73dc"
 # Required: true
 # Possible values: mainnet, testnet, regtest
 # Environment: SIGNER_SIGNER__NETWORK
+# TODO(715): Change to mainnet.
 network = "regtest"
 
 # The address that deployed the sbtc smart contracts.
 #
 # Required: true
+# TODO(715): Change after SCs have been deployed.
 deployer = "SN3R84XZYA63QS28932XQF3G1J8R9PC3W76P9CSQS"
 
 # The signer database endpoint (pgsql connection string)
@@ -136,6 +141,7 @@ db_endpoint = "postgresql://postgres:postgres@localhost:5432/signer"
 # peers who are approved to be in the sBTC signer set.
 #
 # Required: true Environment: SIGNER_SIGNER__BOOTSTRAP_SIGNING_SET
+# TODO(715): Change after initial signing set has been determined.
 bootstrap_signing_set = [
     "035249137286c077ccee65ecc43e724b9b9e5a588e3d7f51e3b62f9624c2a49e46",
     "031a4d9f4903da97498945a4e01a5023a1d53bc96ad670bfe03adf8a06c52e6380",
@@ -147,7 +153,14 @@ bootstrap_signing_set = [
 # `bootstrap_signing_set`. Must be strictly positive.
 # 
 # Required: true Environment: SIGNER_SIGNER__BOOTSTRAP_SIGNATURES_REQUIRED
+# TODO(715): Change as specified by sBTC SIP.
 bootstrap_signatures_required = 2
+
+# Seconds to wait before processing a new Bitcoin block.
+# Required: true Environment: SIGNER_SIGNER__BITCOIN_PROCESSING_DELAY
+# TODO(715): Add default delay (15 seconds? see #701)
+bitcoin_processing_delay = 0
+
 # !! ==============================================================================
 # !! Stacks Event Observer Configuration
 # !!
@@ -183,6 +196,7 @@ bind = "0.0.0.0:8801"
 # Environment: SIGNER_SIGNER__P2P__SEEDS
 # Environment Example: tcp://seed-1:4122,tcp://seed-2:4122
 # TODO(429): Add well-known seed nodes
+# TODO(715): Add well-known seed nodes
 seeds = []
 
 # The local network interface(s) and port(s) to listen on. 

signer/src/config/error.rs
@@ -56,4 +56,7 @@ pub enum SignerConfigError {
     /// Unsupported database driver
     #[error("Unsupported database driver: {0}. Supported drivers are: 'postgresql'.")]
     UnsupportedDatabaseDriver(String),
+
+    #[error("The provided Bitcoin processing delay must be small than {0}s, got {1}s")]
+    InvalidBitcoinProcessingDelay(u64, u64),
 }

signer/src/config/serialization.rs
@@ -33,6 +33,19 @@ where
         .map_err(serde::de::Error::custom)
 }
 
+/// A deserializer for the std::time::Duration type.
+/// Serde includes a default deserializer, but it expects a struct.
+pub fn duration_seconds_deserializer<'de, D>(
+    deserializer: D,
+) -> Result<std::time::Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Ok(std::time::Duration::from_secs(
+        u64::deserialize(deserializer).map_err(serde::de::Error::custom)?,
+    ))
+}
+
 pub fn p2p_multiaddr_deserializer_vec<'de, D>(deserializer: D) -> Result<Vec<Multiaddr>, D::Error>
 where
     D: Deserializer<'de>,

signer/src/testing/transaction_coordinator.rs
@@ -125,7 +125,10 @@ where
     Stacks: StacksInteract + Clone + Sync + Send + 'static,
 {
     /// Assert that a coordinator should be able to coordiante a signing round
-    pub async fn assert_should_be_able_to_coordinate_signing_rounds(mut self) {
+    pub async fn assert_should_be_able_to_coordinate_signing_rounds(
+        mut self,
+        delay_to_process_new_blocks: Duration,
+    ) {
         let mut rng = rand::rngs::StdRng::seed_from_u64(46);
         let network = network::InMemoryNetwork::new();
         let signer_info = testing::wsts::generate_signer_info(&mut rng, self.num_signers as usize);
@@ -243,12 +246,14 @@ where
             .expect("failed to signal");
 
         // Await the `wait_for_tx_task` to receive the first transaction broadcasted.
-        let broadcasted_tx =
-            tokio::time::timeout(Duration::from_secs(10), wait_for_transaction_task)
-                .await
-                .unwrap()
-                .expect("failed to receive message")
-                .expect("no message received");
+        let broadcasted_tx = tokio::time::timeout(
+            delay_to_process_new_blocks + Duration::from_secs(10),
+            wait_for_transaction_task,
+        )
+        .await
+        .unwrap()
+        .expect("failed to receive message")
+        .expect("no message received");
 
         // Extract the first script pubkey from the broadcasted transaction.
         let first_script_pubkey = broadcasted_tx

signer/src/transaction_coordinator.rs
@@ -12,6 +12,7 @@ use futures::FutureExt;
 use futures::StreamExt as _;
 use futures::TryStreamExt;
 use sha2::Digest;
+use tokio::time::sleep;
 use tokio_stream::wrappers::BroadcastStream;
 
 use crate::bitcoin::utxo;
@@ -236,6 +237,12 @@ where
         if !self.is_coordinator(&bitcoin_chain_tip, &signer_public_keys) {
             tracing::debug!("We are not the coordinator, so nothing to do");
             return Ok(());
+        }
+
+        let bitcoin_processing_delay = self.context.config().signer.bitcoin_processing_delay;
+        if bitcoin_processing_delay > std::time::Duration::ZERO {
+            tracing::debug!("Sleeping before processing new Bitcoin block.");
+            sleep(bitcoin_processing_delay).await;
         }
 
         tracing::debug!("We are the coordinator, we may need to coordinate DKG");
@@ -1028,8 +1035,35 @@ mod tests {
     #[tokio::test]
     async fn should_be_able_to_coordinate_signing_rounds() {
         test_environment()
-            .assert_should_be_able_to_coordinate_signing_rounds()
+            .assert_should_be_able_to_coordinate_signing_rounds(std::time::Duration::ZERO)
             .await;
+    }
+
+    #[tokio::test]
+    async fn should_wait_before_processing_bitcoin_blocks() {
+        // NOTE: Above test `should_be_able_to_coordinate_signing_rounds`
+        // could be removed as redundant now.
+
+        // Measure baseline.
+        let baseline_start = std::time::Instant::now();
+        test_environment()
+            .assert_should_be_able_to_coordinate_signing_rounds(std::time::Duration::ZERO)
+            .await;
+        // Locally this takes a couple seconds to execute.
+        // This truncates the decimals.
+        let baseline_elapsed = std::time::Duration::from_secs(baseline_start.elapsed().as_secs());
+
+        let delay_i = 3;
+        let delay = std::time::Duration::from_secs(delay_i);
+        std::env::set_var(
+            "SIGNER_SIGNER__BITCOIN_PROCESSING_DELAY",
+            delay_i.to_string(),
+        );
+        let start = std::time::Instant::now();
+        test_environment()
+            .assert_should_be_able_to_coordinate_signing_rounds(delay)
+            .await;
+        more_asserts::assert_gt!(start.elapsed(), delay + baseline_elapsed);
     }
 
     #[tokio::test]

commit 62414d77a5d321a0189ea259d81c3df56219c794

fix: linter failing after #714 (#717)
* Rename constant and add definition

* Praise the linter

* No need to clone either

signer/src/config/mod.rs
@@ -24,7 +24,8 @@ use crate::stacks::wallet::SignerWallet;
 mod error;
 mod serialization;
 
-pub const MAX_BITCOIN_PROCESSING_DELAY_S: u64 = 300;
+/// Maximum configurable delay (in seconds) before processing new Bitcoin blocks.
+pub const MAX_BITCOIN_PROCESSING_DELAY_SECONDS: u64 = 300;
 
 /// Trait for validating configuration values.
 trait Validatable {
@@ -269,10 +270,10 @@ impl Validatable for SignerConfig {
         }
 
         let delay_secs = cfg.signer.bitcoin_processing_delay.as_secs();
-        if delay_secs > MAX_BITCOIN_PROCESSING_DELAY_S {
+        if delay_secs > MAX_BITCOIN_PROCESSING_DELAY_SECONDS {
             return Err(ConfigError::Message(
                 SignerConfigError::InvalidBitcoinProcessingDelay(
-                    MAX_BITCOIN_PROCESSING_DELAY_S,
+                    MAX_BITCOIN_PROCESSING_DELAY_SECONDS,
                     delay_secs,
                 )
                 .to_string(),
@@ -655,14 +656,14 @@ mod tests {
     fn invalid_bitcoin_processing_delay_returns_correct_error() {
         clear_env();
 
-        let delay = MAX_BITCOIN_PROCESSING_DELAY_S + 1;
+        let delay = MAX_BITCOIN_PROCESSING_DELAY_SECONDS + 1;
         std::env::set_var("SIGNER_SIGNER__BITCOIN_PROCESSING_DELAY", delay.to_string());
 
         let settings = Settings::new_from_default_config();
         assert!(settings.is_err());
         assert!(matches!(
             settings.unwrap_err(),
-            ConfigError::Message(msg) if msg == SignerConfigError::InvalidBitcoinProcessingDelay(MAX_BITCOIN_PROCESSING_DELAY_S, delay).to_string()
+            ConfigError::Message(msg) if msg == SignerConfigError::InvalidBitcoinProcessingDelay(MAX_BITCOIN_PROCESSING_DELAY_SECONDS, delay).to_string()
         ));
     }
 

signer/src/main.rs
@@ -369,14 +369,14 @@ async fn run_transaction_signer(ctx: impl Context) -> Result<(), Error> {
 
 /// Run the transaction coordinator event-loop.
 async fn run_transaction_coordinator(ctx: impl Context) -> Result<(), Error> {
-    let private_key = ctx.config().signer.private_key.clone();
+    let private_key = ctx.config().signer.private_key;
     let network = P2PNetwork::new(&ctx);
 
     let coord = transaction_coordinator::TxCoordinatorEventLoop {
         network,
         context: ctx,
         context_window: 10000,
-        private_key: private_key,
+        private_key,
         signing_round_max_duration: Duration::from_secs(10),
         threshold: 2,
         dkg_max_duration: Duration::from_secs(10),